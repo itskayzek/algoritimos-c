7b11f919c7f4c589ca852515fa61e3476a5be972
itskayzek <tiolzinn@gmail.com>
2026-05-16 18:52:33 -0300

    Atualizar o exercicio-21_positivoOUnegativo.c

--- decisoes/exercicio-21_positivoOUnegativo.c
+++ decisoes/exercicio-21_positivoOUnegativo.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int numero;
+    printf("Digite um número: ");
+    scanf("%i", &numero);
+    if (numero > 0)
+      printf("Esse número é positivo!");
+    else if (numero < 0)
+      printf("Esse número é negativo!");
+    else
+      printf("Esse número é 0!");
+    return 0;
+}
