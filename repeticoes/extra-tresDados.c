e57e5e6fc08bc22e3f1946f96e03ae4b64426387
itskayzek <tiolzinn@gmail.com>
2026-05-17 22:45:39 -0300

    Atualizar o extra-tresDados.c

--- repeticoes/extra-tresDados.c
+++ repeticoes/extra-tresDados.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int somaDados, contador1, contador2, contador3;
+    scanf("%i", &somaDados);
+    
+    for (contador1 = 1; contador1 <= 6; contador1++) {
+      for (contador2 = 1; contador2 <= 6; contador2++) {
+        for (contador3 = 1; contador3 <= 6; contador3++) {        
+          if (somaDados == (contador1 + contador2 + contador3))
+            printf("%i %i %i\n", contador1, contador2, contador3);
+        }
+      }
+    }
+  return 0;
+}
