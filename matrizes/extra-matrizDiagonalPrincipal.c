6c4e697e0a697fd262956d8c46ac4d040bd7a118
itskayzek <tiolzinn@gmail.com>
2026-06-30 22:59:57 -0300

    Criar extra-matrizDiagonalPrincipal.c

---
+++ matrizes/extra-matrizDiagonalPrincipal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define dim 5
+
+int main() {
+    int icont, jcont, matriz[dim][dim];
+    
+    for (icont = 0; icont < dim; icont++){
+      for (jcont = 0; jcont < dim; jcont++){
+        if (icont == jcont)
+          matriz[icont][jcont] = 1;
+        else
+          matriz[icont][jcont] = 0;
+      }
+    }
+    
+    for (icont = 0; icont < dim; icont++){
+      for (jcont = 0; jcont < dim; jcont++){
+        printf("%i ", matriz[icont][jcont]);
+      }
+      printf("\n");
+    }
+    
+    return 0;
+}
